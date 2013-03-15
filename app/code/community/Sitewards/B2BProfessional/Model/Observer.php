a488d4189ec08e51f090696b5e0eef7ac78f9e56
David Manners <dmanners87@gmail.com>
2013-03-15 11:09:21 +0100

    Add check for customer login page
    Add a check to see if the user is logged in.
    Alse check for Mage_Customer_AccountController to allow for users to
    register/login.

--- app/code/community/Sitewards/B2BProfessional/Model/Observer.php
+++ app/code/community/Sitewards/B2BProfessional/Model/Observer.php
@@ -19,13 +19,26 @@ class Sitewards_B2BProfessional_Model_Observer {
 		/* @var $oHelper Sitewards_B2BProfessional_Helper_Data */
 		$oHelper = Mage::helper('b2bprofessional');
 		if($oHelper->checkGlobalActive() == true) {
-			if($oHelper->checkRequireLogin() == true) {
+			/*
+			 * Check to see if the system requires a login
+			 * And there is no logged in user
+			 */
+			if($oHelper->checkRequireLogin() == true && !Mage::getSingleton('customer/session')->isLoggedIn()) {
 				/* @var $oControllerAction Mage_Core_Controller_Front_Action */
 				$oControllerAction = $oObserver->getData('controller_action');
 				/*
-				 * Check to see if the controller is Cms related
+				 * Check to see if the controller is:
+				 * 	1) Cms related for cms pages,
+				 * 	2) A front action to allow for admin pages,
+				 * 	3) Customer account to allow for login
 				 */
-				if(!$oControllerAction instanceof Mage_Cms_IndexController && $oControllerAction instanceof Mage_Core_Controller_Front_Action){
+				if(
+					!$oControllerAction instanceof Mage_Cms_IndexController
+						&&
+					$oControllerAction instanceof Mage_Core_Controller_Front_Action
+						&&
+					!$oControllerAction instanceof Mage_Customer_AccountController
+				){
 					// Redirect to the homepage
 					/* @var $oResponse Mage_Core_Controller_Response_Http */
 					$oResponse = $oControllerAction->getResponse();
