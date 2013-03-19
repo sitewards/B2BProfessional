f72626ae03382ddfc9b14d8a50097aa2a79f1edf
David Manners <dmanners87@gmail.com>
2013-03-19 16:50:58 +0100

    Remove catalog block rewrites
    Remove the catalog block rewrites to use a couple of event observers.

--- app/code/community/Sitewards/B2BProfessional/Model/Observer.php
+++ app/code/community/Sitewards/B2BProfessional/Model/Observer.php
@@ -9,6 +9,13 @@
  */
 class Sitewards_B2BProfessional_Model_Observer {
 	/**
+	 * The last product Id
+	 *
+	 * @var int
+	 */
+	protected static $_iLastProductId = 0;
+
+	/**
 	 * Check if the site requires login to work
 	 * 	- Add notice,
 	 * 	- Redirect to the home page,
@@ -57,6 +64,63 @@ class Sitewards_B2BProfessional_Model_Observer {
 					session_write_close();
 				}
 			}
+		}
+	}
+
+	/**
+	 * Check for block Mage_Catalog_Block_Product_Price
+	 * 	- Check the product is active via the Sitewards_B2BProfessional_Helper_Data
+	 * 	- Replace the text with that on the b2bprofessional
+	 *
+	 * @param Varien_Event_Observer $oObserver
+	 * @return string
+	 */
+	public function onCoreBlockAbstractToHtmlAfter(Varien_Event_Observer $oObserver) {
+		$oBlock = $oObserver->getData('block');
+		$oTransport = $oObserver->getData('transport');
+
+		if($oBlock instanceof Mage_Catalog_Block_Product_Price) {
+			$oProduct = $oBlock->getProduct();
+			$iCurrentProductId = $oProduct->getId();
+
+			/* @var $oB2BHelper Sitewards_B2BProfessional_Helper_Data */
+			$oB2BHelper = Mage::helper('b2bprofessional');
+
+			if ($oB2BHelper->checkActive($iCurrentProductId)) {
+				// To stop duplicate information being displayed validate that we only do this once per product
+				if ($iCurrentProductId != self::$_iLastProductId) {
+					self::$_iLastProductId = $iCurrentProductId;
+
+					// text displayed instead of price
+					if (Mage::getStoreConfig('b2bprofessional/languagesettings/languageoverride') == 1) {
+						$sReplacementText = Mage::getStoreConfig('b2bprofessional/languagesettings/logintext');
+					} else {
+						$sReplacementText = $oB2BHelper->__('Please login');
+					}
+
+					$oTransport->setHtml($sReplacementText);
+				} else {
+					$oTransport->setHtml('');
+				}
+				// Set can show price to false to stop tax being displayed via Symmetrics_TweaksGerman_Block_Tax
+				$oProduct->setCanShowPrice(false);
+			}
+		}
+	}
+
+	/**
+	 * On the event catalog_product_type_configurable_price
+	 * Set the COnfigurable price of a product to 0 to stop the changed price showing up in the drop down
+	 *
+	 * @param Varien_Event_Observer $oObserver
+	 */
+	public function onCatalogProductTypeConfigurablePrice(Varien_Event_Observer $oObserver) {
+		$oProduct = $oObserver->getData('product');
+		/* @var $oB2BHelper Sitewards_B2BProfessional_Helper_Data */
+		$oB2BHelper = Mage::helper('b2bprofessional');
+
+		if ($oB2BHelper->checkActive($oProduct->getId())) {
+			$oProduct->setConfigurablePrice(0);
 		}
 	}
 }
