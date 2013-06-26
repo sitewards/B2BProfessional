5b4b3fa44ac7b7137ea044ec3d7dfbb141492916
Oleksii Svarychevskyi <a.ekspo@gmail.com>
2013-06-26 10:16:37 +0200

    #SBBP-27 added travis config and one unit test

---
+++ app/code/community/Sitewards/B2BProfessional/Test/Helper/Data.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Test for class Sitewards_B2BProfessional_Helper_Data
+ *
+ * @category    Sitewards
+ * @package     Sitewards_B2BProfessional
+ * @copyright   Copyright (c) 2013 Sitewards GmbH (http://www.sitewards.com/)
+ */
+class Sitewards_B2BProfessional_Test_Helper_Data extends EcomDev_PHPUnit_Test_Case
+{
+	/**
+	 * Tests is extension active
+	 *
+	 * @test
+	 * @loadFixture
+	 */
+	public function testIsExtensionActive()
+	{
+		$this->assertTrue(Mage::helper('b2bprofessional')->isExtensionActive());
+	}
+}
