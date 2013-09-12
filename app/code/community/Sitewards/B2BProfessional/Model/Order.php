f33eb4d1ee4395179fc5c40466eb1eb324f2f4a4
airbone42 <tobias.zander@sitewards.com>
2013-09-12 18:54:35 +0200

    added a cancel option

--- app/code/community/Sitewards/B2BProfessional/Model/Order.php
+++ app/code/community/Sitewards/B2BProfessional/Model/Order.php
@@ -21,4 +21,19 @@ class Sitewards_B2BProfessional_Model_Order extends Mage_Core_Model_Abstract{
 	public function getByOrder($iOrderId, $sKey = '') {
 		return $this->_getResource()->getByOrder($iOrderId, $sKey);
 	}
+
+	/**
+	 * @param int $iOrderId
+	 * @return bool
+	 */
+	public function canCancel($iOrderId) {
+		/* @var $oOrder Mage_Sales_Model_Order */
+		$oOrder = Mage::getModel('sales/order')->load($iOrderId);
+		if ($oOrder->getCustomerId() == Mage::getSingleton('customer/session')->getCustomerId()) {
+			$sDeliveryDate = current($this->getByOrder($iOrderId, 'delivery_date'));
+			return (strtotime($sDeliveryDate) > now());
+		} else {
+			return false;
+		}
+	}
 }
--- app/code/community/Sitewards/B2BProfessional/controllers/OrderController.php
+++ app/code/community/Sitewards/B2BProfessional/controllers/OrderController.php
@@ -12,4 +12,15 @@ class Sitewards_B2BProfessional_OrderController extends Mage_Core_Controller_Fro
 		$this->loadLayout();
 		$this->renderLayout();
 	}
+
+	public function cancelAction() {
+		$iOrderId = $this->getRequest()->getParam('id');
+		$oB2BModel = Mage::getModel('b2bprofessional/order');
+		$oMageModel = Mage::getModel('sales/order')->load($iOrderId);
+		$oSession = Mage::getSingleton('checkout/session');;
+		if ($oB2BModel->canCancel($iOrderId)) {
+			$oMageModel->setState(Mage_Sales_Model_Order::STATE_CANCELED, true)->save();
+		}
+		$this->getResponse()->setRedirect(Mage::helper('b2bprofessional')->getOrderHistoryUrl());
+	}
 }
