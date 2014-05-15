3fbc26ef2a22d4b8b6d79d6246444239ef380c3f
David Manners <dmanners87@gmail.com>
2014-05-15 14:50:33 +0200

    Update test to work with new helper

--- app/code/community/Sitewards/B2BProfessional/Test/Helper/Data.php
+++ app/code/community/Sitewards/B2BProfessional/Test/Helper/Data.php
@@ -1,24 +1,25 @@
 <?php
+
 /**
  * Test for class Sitewards_B2BProfessional_Helper_Data
  *
  * @category    Sitewards
  * @package     Sitewards_B2BProfessional
- * @copyright   Copyright (c) 2013 Sitewards GmbH (http://www.sitewards.com/)
+ * @copyright   Copyright (c) 2014 Sitewards GmbH (http://www.sitewards.com/)
  */
 class Sitewards_B2BProfessional_Test_Helper_Data extends EcomDev_PHPUnit_Test_Case
 {
-	/**
-	 * Tests is extension active
-	 *
-	 * @test
-	 * @loadFixture
-	 */
-	public function testIsExtensionActive()
-	{
-		$this->assertTrue(
-			Mage::helper('b2bprofessional')->isExtensionActive(),
-			"Extension is not active please check config"
-		);
-	}
+    /**
+     * Tests is extension active
+     *
+     * @test
+     * @loadFixture
+     */
+    public function testIsExtensionActive()
+    {
+        $this->assertTrue(
+            Mage::helper('sitewards_b2bprofessional')->isExtensionActive(),
+            "Extension is not active please check config"
+        );
+    }
 }
