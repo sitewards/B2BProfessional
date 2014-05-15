f39ef3ae3c5ce37dafe099788a8307b4aac172c5
David Manners <dmanners87@gmail.com>
2014-05-15 13:46:21 +0200

    Add check to hide product price

--- app/code/community/Sitewards/B2BProfessional/Model/Observer.php
+++ app/code/community/Sitewards/B2BProfessional/Model/Observer.php
@@ -3,6 +3,23 @@
 class Sitewards_B2BProfessional_Model_Observer
 {
     /**
+     * The last product Id
+     *
+     * @var int
+     */
+    protected static $_iLastProductId = 0;
+
+    /**
+     * blocks which display prices
+     *
+     * @var array
+     */
+    protected $_aPriceBlockClassNames = array(
+        'Mage_Catalog_Block_Product_Price' => 1,
+        'Mage_Bundle_Block_Catalog_Product_Price' => 1,
+    );
+
+    /**
      * Check to see if a product can be sold to the current logged in user
      *
      * @param Varien_Event_Observer $oObserver
@@ -36,5 +53,53 @@ class Sitewards_B2BProfessional_Model_Observer
                 throw new Mage_Catalog_Exception('Please log in to buy items');
             }
         }
+    }
+
+    public function coreBlockAbstractToHtmlAfter(Varien_Event_Observer $oObserver)
+    {
+        $oBlock = $oObserver->getData('block');
+        $oTransport = $oObserver->getData('transport');
+
+        /** @var Sitewards_B2BProfessional_Helper_Data $oB2BHelper */
+        $oB2BHelper = Mage::helper('sitewards_b2bprofessional');
+        if ($oB2BHelper->isExtensionActive() === true) {
+            /*
+             * Check to see if we should remove the product price
+             */
+            if ($this->isExactlyPriceBlock($oBlock)) {
+                $oProduct = $oBlock->getProduct();
+                $iCurrentProductId = $oProduct->getId();
+
+                if ($oB2BHelper->isProductActive($oProduct) === false) {
+                    // To stop duplicate information being displayed validate that we only do this once per product
+                    if ($iCurrentProductId !== self::$_iLastProductId) {
+                        self::$_iLastProductId = $iCurrentProductId;
+
+                        $oTransport->setHtml('Please login to see price');
+                    } else {
+                        $oTransport->setHtml('');
+                    }
+
+                    // Set type id to combined to stop tax being displayed via Symmetrics_TweaksGerman_Block_Tax
+                    if (
+                        Mage::helper('core')->isModuleEnabled('Symmetrics_TweaksGerman')
+                        && $oProduct->getTypeId() == 'bundle'
+                    ) {
+                        $oProduct->setTypeId('combined');
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * checks if the block represents a price block
+     *
+     * @param Mage_Core_Block_Abstract $oBlock
+     * @return bool
+     */
+    protected function isExactlyPriceBlock($oBlock)
+    {
+        return ($oBlock && isset($this->_aPriceBlockClassNames[get_class($oBlock)]));
     }
 }
