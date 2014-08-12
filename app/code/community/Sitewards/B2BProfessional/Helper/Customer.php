ba082befbb0d34965fd096e0c8076d270b32bb05
David Manners <dmanners87@gmail.com>
2014-08-12 12:27:57 +0200

    Add global customer activation check back from version 2.2.5

--- app/code/community/Sitewards/B2BProfessional/Helper/Customer.php
+++ app/code/community/Sitewards/B2BProfessional/Helper/Customer.php
@@ -28,6 +28,11 @@ class Sitewards_B2BProfessional_Helper_Customer extends Sitewards_B2BProfessiona
     const CONFIG_EXTENSION_ACTIVE_CUSTOMER_GROUPS = 'b2bprofessional/activatebycustomersettings/activecustomers';
 
     /**
+     * Path for the config for customer groups that are activated
+     */
+    const CONFIG_EXTENSION_ACTIVE_CUSTOMER_GLOBALLY = 'b2bprofessional/generalsettings/activecustomers';
+
+    /**
      * Flag if the extension is set to require login
      *
      * @var bool
@@ -50,12 +55,55 @@ class Sitewards_B2BProfessional_Helper_Customer extends Sitewards_B2BProfessiona
 
     /**
      * Check to see if the customer is logged in
+     *  - Is the session logged in,
+     *  - Are customers activated globally,
+     *  - Is this one customer active for the current store,
      *
      * @return bool
      */
     public function isCustomerLoggedIn()
     {
-        return Mage::helper('customer')->isLoggedIn();
+        if (Mage::helper('customer')->isLoggedIn() === true) {
+            /*
+             * If customer activation is global
+             *  - then any customer can access any store
+             */
+            if ($this->isCustomerActivationGlobal()) {
+                return true;
+            }
+
+            /* @var $oCustomerSession Mage_Customer_Model_Session */
+            $oCustomerSession = Mage::getSingleton('customer/session');
+            /* @var $oCustomer Mage_Customer_Model_Customer */
+            $oCustomer = $oCustomerSession->getCustomer();
+            if($this->isCustomerActiveForStore($oCustomer)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Check if the extension has customers activated globally
+     *
+     * @return bool
+     */
+    private function isCustomerActivationGlobal()
+    {
+        return Mage::getStoreConfigFlag(self::CONFIG_EXTENSION_ACTIVE_CUSTOMER_GLOBALLY);
+    }
+
+    /**
+     * Check if a given customer is allowed for the current store
+     *
+     * @param Mage_Customer_Model_Customer $oCustomer
+     * @return bool
+     */
+    private function isCustomerActiveForStore(Mage_Customer_Model_Customer $oCustomer)
+    {
+        $oCustomerStore = $oCustomer->getStore();
+        $iStoreId       = $oCustomerStore->getId();
+        return $iStoreId === Mage::app()->getStore()->getId();
     }
 
     /**
